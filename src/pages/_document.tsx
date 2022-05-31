717aecc10b3acf611cf82edc004e3b8ae9e42425
ade1705 <damdey@gmail.com>
2022-05-31 19:55:41 -0400

    feat(portfolio): initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
